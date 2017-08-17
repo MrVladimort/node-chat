3330ec000cf8147c1cbf294f63da5691f4f07e12
mrvladimort <mrvladimort@gmail.com>
2017-08-17 02:54:19 +0200

    up

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.post('/register', require('./register').post);
 router.get('/register', require('./register').get);
 router.get('/verify-email/:verifyEmailToken', require('./verifyEmail').get);
 router.get('/private', require('./private').get);
+router.get('/auth/vk', require('./vk').get);
+router.get('/auth/google', require('./google').get);
+router.get('/auth/facebook', require('./facebook').get);
 
 module.exports = router.routes();
