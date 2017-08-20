1e2fd7c7c8a0c58a65f95c4d2893eb2095d0a6b1
mrvladimort <mrvladimort@gmail.com>
2017-08-20 21:20:40 +0200

    up

--- routes/callback/facebook.js
+++ routes/callback/facebook.js
@@ -1,5 +1,9 @@
 const passport = require('koa-passport');
 
-exports.get = async function(ctx, next) {
-    passport.authenticate('facebook')(ctx, next);
+exports.get = async function (ctx, next) {
+    passport.authenticate('facebook', {
+        successRedirect: '/',
+        failureRedirect: '/',
+        failureFlash: true // req.flash
+    })(ctx, next);
 };
