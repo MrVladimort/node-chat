3c734b25756c904dbe3b8c1f6d8585509df2b102
mrvladimort <mrvladimort@gmail.com>
2017-08-26 14:12:18 +0200

    up

--- libs/passport/authenticateByProfile.js
+++ libs/passport/authenticateByProfile.js
@@ -59,7 +59,7 @@ module.exports = async function (req, profile, done) {
 
         // works?
     let error = await user.validate();
-
+    console.log(error);
     if(error){
         throw new UserAuthError("Недостаточно данных или пользователь с таким именнем " +
             "зарегестрирован на другой Email адрес.");
